fc347d6964ac223bbf2eee5cbf32c31061e25885
guepardo <bsinet@hotmail.com>
2016-11-14 14:38:51 -0200

    LogParser.class: refatorando __chunk_is_important para que se retorne um resultado mais útil. Novo função __new_game_dictionary que retorna um template para o parse. Estrutura de decisão no método parse.

--- lib/quake3.py
+++ lib/quake3.py
@@ -10,18 +10,36 @@ class LogParser:
 		#Casocontrário, o método __check_file_exists lança uma exceção
 		self.__check_file_exists()
 
+		#Armazena a referência de qual partida o parser está processando
+		temp_game_round = 0
+		
+		log_parsed = {}
+
 		for chunk in self.__chunk_lines(): 
-			print(chunk)
+			# print(chunk)
+
+			if chunk['type'] == 'Kill:': 
+				#dosomething here
+				continue
+
+			if chunk['type'] == 'InitGame:': 
+				#dosomething here
+				self.log_parsed['game-'+ temp_game_round] = self.__new_game_dictionary()
+				continue
+
+			if chunk['type'] == 'ShutdownGame:': 
+				#dosomething here
+				continue
 
 
 	def __chunk_lines(self): 
 		log = open(self.__log_file_path, 'r')
 
 		for line in log.readlines():
-			if not self.__chunk_is_important(line): 
+			chunk = self.__chunk_is_important(line); 
+			if not chunk['status']: 
 				continue
-
-			yield line
+			yield chunk['status']
 
 	#Verifica se a fatia do logo é importante para o contexto do parser
 	def __chunk_is_important(self, chunk):
@@ -34,11 +52,20 @@ class LogParser:
 
 		for label in important_labels:
 			if label in chunk:
-				return True
+				return {'status': True, 'type': label, 'content': chunk }
 
-		return False
+		return {'status': False }; 
 
 	#Analisa o caminho do arquivo e lança uma Exception caso seja necessário
 	def __check_file_exists(self):
 		if not os.path.isfile(self.__log_file_path):
 			raise Exception('Path informado não é um arquivo', self.__log_file_path); 
+
+	def __new_game_dictionary(self):
+		d = {
+			'total_kills'    : 0,
+			'players'	     : [], 
+			'kills'          : {}, 
+			'kills_by_means' : {}
+		}
+		return d
