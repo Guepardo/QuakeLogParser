606667dff0ad966d8a2f878115242902a71afb87
guepardo <bsinet@hotmail.com>
2016-11-14 13:57:20 -0200

    LogParser.class: novo função __chunk_is_important (Verifica se a linha do log é important para o contexto do parser)

--- lib/quake3.py
+++ lib/quake3.py
@@ -1,6 +1,8 @@
 import os
 
 class LogParser:
+	
+
 	def parse(self, fullpath=''):
 		self.__log_file_path = fullpath
 		
@@ -11,12 +13,32 @@ class LogParser:
 		for chunk in self.__chunk_lines(): 
 			print(chunk)
 
+
 	def __chunk_lines(self): 
 		log = open(self.__log_file_path, 'r')
 
 		for line in log.readlines():
+			if not self.__chunk_is_important(line): 
+				continue
+
 			yield line
 
+	#Verifica se a fatia do logo é importante para o contexto do parser
+	def __chunk_is_important(self, chunk):
+		#pedaços que são importantes contêm
+		#Kill: 			(representa um assassinato no jogo)
+		#InitGame:      (representa o início do jogo/partida)
+		#ShutdownGame:	(representa que o jogo foi finalizado)
+
+		important_labels = ['Kill:', 'InitGame:', 'ShutdownGame:']
+
+		for label in important_labels:
+			if label in chunk:
+				return True
+
+		return False
+
+	#Analisa o caminho do arquivo e lança uma Exception caso seja necessário
 	def __check_file_exists(self):
 		if not os.path.isfile(self.__log_file_path):
 			raise Exception('Path informado não é um arquivo', self.__log_file_path); 
